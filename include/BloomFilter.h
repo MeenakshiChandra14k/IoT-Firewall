7bad581a564403588de89c8268cdf5f47490459c
Sajith CS <meenakshichandra14k@gmail.com>
2026-04-18 20:47:30 +0530

    Upgrade: Implement Counting Bloom Filter with thread-safe shared_mutex

--- include/BloomFilter.h
+++ include/BloomFilter.h
@@ -5,16 +5,20 @@
 #include <vector>
 #include <string>
 #include <functional>
+#include <shared_mutex>
 
 class BloomFilter {
     private:
-        std::vector<bool> bit_array;
+        std::vector<uint8_t> counts;
+        //std::vector<bool> bit_array;
         size_t size;
         int num_hashes;
+        mutable std::shared_mutex mtx;
 
     public:
         BloomFilter(size_t s, int h);
         void add(const std::string& item);
+        void remove(const std::string& item);
         bool contains(const std::string& item) const;
 
 };
--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -4,10 +4,12 @@
 
 
 BloomFilter::BloomFilter(size_t s, int h) : size(s), num_hashes(h) {
-    bit_array.resize(size, false);
+    counts.resize(size,0);
+    //bit_array.resize(size, false);
 }
 
 void BloomFilter:: add(const std::string& item) {
+    std::unique_lock lock(mtx);
     uint64_t hash_out[2];
 
     MurmurHash3_x64_128(item.c_str(), (int)item.length(), 42, hash_out);
@@ -17,11 +19,30 @@ void BloomFilter:: add(const std::string& item) {
 
     for(int i=0; i<num_hashes; i++){
         size_t index = (h1 + i*h2) % size;
-        bit_array[index] = true;
+        if(counts[index]<255) counts[index]++;
+        //bit_array[index] = true;
     }
 }
 
+void BloomFilter:: remove(const std:: string& item){
+    std::unique_lock lock(mtx);
+    uint64_t hash_out[2];
+
+    MurmurHash3_x64_128(item.c_str(), (int)item.length(), 42, hash_out);
+
+    uint64_t h1 = hash_out[0];
+    uint64_t h2 = hash_out[1];
+
+    for(int i=0; i<num_hashes; i++){
+        size_t index = (h1 + i*h2) % size;
+        if(counts[index] > 0) counts[index]--;
+    }
+}
+
+
+
 bool BloomFilter::contains( const std::string& item) const {
+    std::shared_lock lock(mtx);
     uint64_t hash_out[2];
     MurmurHash3_x64_128(item.c_str(), (int)item.length(), 42, hash_out);
 
@@ -31,9 +52,10 @@ bool BloomFilter::contains( const std::string& item) const {
     for(int i=0; i<num_hashes; i++){
         size_t index = (h1 + i * h2) % size;
 
-        if(!bit_array[index]){
-            return false;
-        }
+        // if(!bit_array[index]){
+        //     return false;
+        // }
+        if(counts[index]==0) return false;
     }
 
     return true;
